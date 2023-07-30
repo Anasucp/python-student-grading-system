5f4f42ccb00409733a16e7454cae73f9f5287499
Muhammad Ans <anas.ccj420@gmail.com>
2023-07-31 03:09:38 +0500

    Add flags and if conditions

--- task1.py
+++ task1.py
@@ -1,36 +1,63 @@
 name = input("Enter your name:")
 Class = input('Enter your class:')
+sum_ = 0
+subject_flag = False
+
 
 #For Class FSCM
 if Class  == 'fscm':
 	print("Enter Your Number:")
-	urdu = input('Urdu:')
-	if urdu > 100:
-		print ("Score must be under 100")
-	pakst = input('Pakst:')
-	if pakst > 100:
-		print ("Score must be under 100")
-	isl = input('Isl:')
-	if isl > 100:
-		print ("Score must be under 100")
-	chem = input('Chem:')
-	if chem > 100:
-		print ("Score must be under 100")
-	bio = input('Bio:')
-	if bio > 100:
-		print ("Score must be under 100")
-	phy = input('Phy:')
-	if phy > 100:
-		print ("Score must be under 100")
-	eng = input('Eng:')
-	if eng > 100:
-		print ("Score must bet under 100")
-	sum_ = int(urdu) + int(pakst) + int(isl) + int(chem) + int(bio) + int(phy) + int(eng)
+	urdu = int(input('Urdu:'))
+	if urdu <= 100 and urdu >= 0:
+		sum_ = sum_ + urdu
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	pakst = int(input('Pakst:'))
+	if pakst <= 100 and pakst >= 0:
+		sum_ = sum_ + pakst
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	isl = int(input('Isl:'))
+	if isl <= 100 and isl >= 0:
+		sum_ = sum_ + isl
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	chem = int(input('Chem:'))
+	if chem <= 100 and chem >= 0:
+		sum_ = sum_ + chem
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	bio = int(input('Bio:'))
+	if bio <= 100 and bio >= 0:
+		sum_ = sum_ + bio
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	phy = int(input('Phy:'))
+	if phy <= 100 and phy >= 0:
+		sum_ = sum_ + phy
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	eng = int(input('Eng:'))
+	if eng <= 100 and eng >= 0:
+		sum_ = sum_ + eng
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+
+
 	percent = (sum_ / 700) * 100
+
 	print (f"Name:{name}")
 	print (f"Class:{Class}")
 	print  (f"Total:{sum_}/700 ")
 	print (f"Percentage:{percent}")
+	
 	if percent > 80:
 		print ("Grade:A")
 
@@ -39,37 +66,62 @@ if Class  == 'fscm':
 
 	else:
 		print ("Grade:F")
+
+	if subject_flag:
+		print("Note: some of your input is incorrect so it will effect on your results.")
 
 #For Class FSCE	
 elif Class  == 'fsce':
 	print("Enter Your Number:")
-	urdu = input('Urdu:')
-	if urdu > 100:
-		print ("Score must be under 100")
-	pakst = input('Pakst:')
-	if pakst > 100:
-		print ("Score must be under 100")
-	isl = input('Isl:')
-	if isl > 100:
-		print ("Score must be under 100")
-	chem = input('Chem:')
-	if chem > 100:
-		print ("Score must be under 100")
-	math = input('math:')
-	if math > 100:
-		print ("Score must be under 100")
-	phy = input('Phy:')
-	if phy > 100:
-		print ("Score must be under 100")
-	eng = input('Eng:')
-	if eng > 100:
-		print ("Score must be under 100")
-	sum_ = int(urdu) + int(pakst) + int(isl) + int(chem) + int(math) + int(phy) + int(eng)
+	urdu = int(input('Urdu:'))
+	if urdu <= 100 and urdu >= 0:
+		sum_ = sum_ + urdu
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	pakst = int(input('Pakst:'))
+	if pakst <= 100 and pakst >= 0:
+		sum_ = sum_ + pakst
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	isl = int(input('Isl:'))
+	if isl <= 100 and isl >= 0:
+		sum_ = sum_ + isl
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	chem = int(input('Chem:'))
+	if chem <= 100 and chem >= 0:
+		sum_ = sum_ + chem
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	math = int(input('math:'))
+	if math <= 100 and math >= 0:
+		sum_ = sum_ + math
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	phy = int(input('Phy:'))
+	if phy <= 100 and phy >= 0:
+		sum_ = sum_ + phy
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	eng = int(input('Eng:'))
+	if eng <= 100 and eng >= 0:
+		sum_ = sum_ + eng
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
 	percent = (sum_ / 700) * 100
+
 	print (f"Name:{name}")
 	print (f"Class:{Class}")
 	print  (f"Total:{sum_}/700 ")
 	print (f"Percentage:{percent}")
+	
 	if percent > 80:
 		print ("Grade:A")
 
@@ -79,36 +131,60 @@ elif Class  == 'fsce':
 	else:
 		print ("Grade:F")
 
+	if subject_flag:
+		print("Note: some of your input is incorrect so it will effect on your results.")
 #For class ICS
 elif Class  == 'ics':
 	print("Enter Your Number:")
-	urdu = input('Urdu:')
-	if urdu > 100:
-		print ("Score must be under 100")
-	pakst = input('Pakst:')
-	if pakst > 100:
-		print ("Score must be under 100")
-	isl = input('Isl:')
-	if isl > 100:
-		print ("Score must be under 100")
-	comp = input('Comp:')
-	if comp> 100:
-		print ("Score must be under 100")
-	math = input('Math:')
-	if map > 100:
-		print ("Score must be under 100")
-	phy = input('Phy:')
-	if phy > 100:
-		print ("Score must be under 100")
-	eng = input('Eng:')
-	if eng > 100:
-		print ("Score must be under 100")
-	sum_ = int(urdu) + int(pakst) + int(isl) + int(comp) + int(Math) + int(phy) + int(eng)
+	urdu = int(input('Urdu:'))
+	if urdu <= 100 and urdu >= 0:
+		sum_ = sum_ + urdu
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	pakst = int(input('Pakst:'))
+	if pakst <= 100 and pakst >= 0:
+		sum_ = sum_ + pakst
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	isl = int(input('Isl:'))
+	if isl <= 100 and isl >= 0:
+		sum_ = sum_ + isl
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	comp = int(input('Comp:'))
+	if comp <= 100 and comp >= 0:
+		sum_ = sum_ + comp
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	math = int(input('Math:'))
+	if math <= 100 and math >= 0:
+		sum_ = sum_ + math
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	phy = int(input('Phy:'))
+	if phy <= 100 and phy >= 0:
+		sum_ = sum_ + phy
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
+	eng = int(input('Eng:'))
+	if eng <= 100 and eng >= 0:
+		sum_ = sum_ + eng
+	else:
+		subject_flag = True
+		sum_ = sum_ + 0
 	percent = (sum_ / 700) * 100
+
 	print (f"Name:{name}")
 	print (f"Class:{Class}")
 	print  (f"Total:{sum_}/700 ")
 	print (f"Percentage:{percent}")
+	
 	if percent > 80:
 		print ("Grade:A")
 
@@ -118,6 +194,8 @@ elif Class  == 'ics':
 	else:
 		print ("Grade:F")
 
+	if subject_flag:
+		print("Note: some of your input is incorrect so it will effect on your results.")
 
 
 
