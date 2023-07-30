f45bdbf46faed92844165ae3e404b39b3aac3210
Muhammad Ans <anas.ccj420@gmail.com>
2023-07-31 01:11:03 +0500

    initial setup and basic usecase

---
+++ task1.py
@@ -0,0 +1,82 @@
+name = input("Enter your name:")
+Class = input('Enter your class:')
+
+#For Class FSCM
+if Class  == 'fscm':
+	print("Enter Your Number:")
+	urdu = int(input('Urdu:'))
+	pakst = int(input('Pakst:'))
+	isl = int(input('Isl:'))
+	chem = int(input('Chem:'))
+	bio = int(input('Bio:'))
+	phy = int(input('Phy:'))
+	eng = int(input('Eng:'))
+	Sum = int(urdu) + int(pakst) + int(isl) + int(chem) + int(bio) + int(phy) + int(eng)
+	percent = (Sum / 700) * 100
+	print (f"Name:{name}")
+	print (f"Class:{Class}")
+	print  (f"Total:{Sum}/700 ")
+	print (f"Percentage:{percent}")
+	if percent > 80:
+		print ("Grade:A")
+
+	elif percent > 50:
+		print ("Grade:B")
+
+	else:
+		print ("Grade:F")
+
+#For Class FSCE	
+elif Class  == 'fsce':
+	print("Enter Your Number:")
+	urdu = int(input('Urdu:'))
+	pakst = int(input('Pakst:'))
+	isl = int(input('Isl:'))
+	chem = int(input('Chem:'))
+	math = int(input('math:'))
+	phy = int(input('Phy:'))
+	eng = int(input('Eng:'))
+	Sum = int(urdu) + int(pakst) + int(isl) + int(chem) + int(math) + int(phy) + int(eng)
+	percent = (Sum / 700) * 100
+	print (f"Name:{name}")
+	print (f"Class:{Class}")
+	print  (f"Total:{Sum}/700 ")
+	print (f"Percentage:{percent}")
+	if percent > 80:
+		print ("Grade:A")
+
+	elif percent > 50:
+		print ("Grade:B")
+
+	else:
+		print ("Grade:F")
+
+#For class ICS
+elif Class  == 'ics':
+	print("Enter Your Number:")
+	urdu = int(input('Urdu:'))
+	pakst = int(input('Pakst:'))
+	isl = int(input('Isl:'))
+	chem = int(input('Chem:'))
+	bio = int(input('Bio:'))
+	phy = int(input('Phy:'))
+	eng = int(input('Eng:'))
+	Sum = int(urdu) + int(pakst) + int(isl) + int(chem) + int(bio) + int(phy) + int(eng)
+	percent = (Sum / 700) * 100
+	print (f"Name:{name}")
+	print (f"Class:{Class}")
+	print  (f"Total:{Sum}/700 ")
+	print (f"Percentage:{percent}")
+	if percent > 80:
+		print ("Grade:A")
+
+	elif percent > 50:
+		print ("Grade:B")
+
+	else:
+		print ("Grade:F")
+
+
+
+
+
